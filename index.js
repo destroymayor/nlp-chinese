8a268e905374de4d7a2eae99a14a2dafafeca376
Jared <kuil5230@gmail.com>
2018-09-26 20:20:29 +0800

    .

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ import stringSimilarity from "string-similarity";
 // replace function
 const replaceCumulative = (Sentence, FindList, ReplaceList, word) => {
   if (word == "n") {
-    for (let i = 0; i < FindList.length; i++) Sentence = Sentence.replace(new RegExp(FindList[i]), "(" + ReplaceList[i] + ")");
+    for (let i = 0; i < FindList.length; i++) Sentence = Sentence.replace(new RegExp(FindList[i]), "" + ReplaceList[i] + "");
     return Sentence;
   }
   if (word == "v") {
-    for (let i = 0; i < FindList.length; i++) Sentence = Sentence.replace(new RegExp(FindList[i]), "[" + ReplaceList[i] + "]");
+    for (let i = 0; i < FindList.length; i++) Sentence = Sentence.replace(new RegExp(FindList[i]), "" + ReplaceList[i] + "");
     return Sentence;
   }
 };
@@ -39,7 +39,6 @@ const replaceCumulative = (Sentence, FindList, ReplaceList, word) => {
 const KeywordCombinationReplaceAll = (ReplacedSentenceFile, CombinedWordFile, output, GenerationsNumber) => {
   //產生句數量計數
   let num = 0;
-
   fs.readFile(ReplacedSentenceFile, "utf-8", (BeingReplaceError, BeingReplaceData) => {
     const BeingReplaceList = JSON.parse(BeingReplaceData);
     fs.readFile(CombinedWordFile, "utf-8", (BecomeDataError, BecomeData) => {
@@ -87,13 +86,13 @@ const KeywordCombinationReplaceAll = (ReplacedSentenceFile, CombinedWordFile, ou
                     "v"
                   );
 
-                  fs.appendFileSync(
-                    output,
-                    "N:" + item.article_title + ", Q: " + ResultSentenceVerb + ", A: " + BecomeDataItemValue.sourceText + "\n",
-                    err => {
-                      if (err) throw err;
-                    }
-                  );
+                  // fs.appendFileSync(
+                  //   output,
+                  //   // "N:" + item.article_title + ", Q: " + ResultSentenceVerb + ", A: " + BecomeDataItemValue.sourceText + "\n",
+                  //   err => {
+                  //     if (err) throw err;
+                  //   }
+                  // );
 
                   //控制產生數量
                   num++;
@@ -110,7 +109,7 @@ const KeywordCombinationReplaceAll = (ReplacedSentenceFile, CombinedWordFile, ou
   });
 };
 
-KeywordCombinationReplaceAll("./file/dataPTT.json", "./file/Combination.json", "./file/output/replacePTT.txt", 10000);
+KeywordCombinationReplaceAll("./file/data291.json", "./file/Combination.json", "./file/output/QA.txt", 10000);
 
 // 收斂 尋找相似句子
 const SearchSimilarSentences = (GenerateSentenceFile, ReferenceSentenceFile) => {
